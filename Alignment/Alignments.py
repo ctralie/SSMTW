Trying out my new idea with merge trees, and fixing a bug with IBDTW backtracing numerical precision that this exposed

Alignment/Alignments.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import scipy.io as sio
 import Alignment._SequenceAlignment as SAC
 from Alignment.AlignmentTools import *
+from Alignment.MergeTree import *
 import Alignment
 from multiprocessing import Pool as PPool
 import time
@@ -90,13 +91,11 @@ def DTWCSM(CSM, doBacktrace = True):
             dul = d + D[i-1, j-1]
             dl = d + D[i, j-1]
             du = d + D[i-1, j]
-            D[i, j] = min(min(dul, dl), du)
-            if dul == D[i, j]:
-                backpointers[(i, j)].append([i-1, j-1, False])
-            if dl == D[i, j]:
-                backpointers[(i, j)].append([i, j-1, False])
-            if du == D[i, j]:
-                backpointers[(i, j)].append([i-1, j, False])
+            arr = np.array([dul, dl, du])
+            idx = np.argmin(arr)
+            D[i, j] = arr[idx]
+            opts = [[i-1, j-1, False], [i, j-1, False], [i-1, j, False]]
+            backpointers[(i, j)].append(opts[idx])
     path = []
     if doBacktrace:
         Backtrace(backpointers, (M, N), path) #Recursive backtrace from the end
@@ -173,7 +172,7 @@ def doIBDTWHelper(args):
 
 def doIBDTW(SSMA, SSMB, NThreads = 8, Verbose = False):
     """
-    Do partial isometry blind dynamic time warping between two
+    Do isometry blind dynamic time warping between two
     self-similarity matrices
     :param SSMA: MXM self-similarity matrix
     :param SSMB: NxN self-similarity matrix
@@ -185,7 +184,6 @@ def doIBDTW(SSMA, SSMB, NThreads = 8, Verbose = False):
     M = SSMA.shape[0]
     N = SSMB.shape[0]
     D = np.zeros((M, N))
-    parpool = None
     if NThreads > 1:
         if not Alignment.parpool:
             Alignment.parpool = PPool(NThreads)
@@ -200,6 +198,57 @@ def doIBDTW(SSMA, SSMB, NThreads = 8, Verbose = False):
         if Verbose:
             print("Finished row %i of %i"%(i, M))
     return D
+
+def doIBDTWMergeTreesHelper(args):
+    (x, res) = args
+    (MT, PS, I) = mergeTreeFrom1DTimeSeries(x)
+    ret = getPersistenceImage(I, [-0.1, 1.1, -0.1, 1.1], res, psigma = res*2)
+    return ret['PI'].flatten()
+
+def doIBDTWMergeTrees(SSMA, SSMB, res, NThreads = 8, Verbose = False):
+    """
+    Do a version of isometry blind time warping between SSMs
+    which uses merge trees as a proxy for constrained DTW
+    between each pair of rows
+    :param SSMA: MXM self-similarity matrix
+    :param SSMB: NxN self-similarity matrix
+    :param res: Width of each pixel in the persistence image
+    :param NThreads: How many threads to use in parallelization of
+        row computation of CSWM
+    :param Verbose: Whether or not to print which row is being aligned
+    :returns {'X': An MxNPixels array of persistence images for SSM A
+              'Y': An NxNPixels array of persistence images for SSM B
+              'D': MxN cross-similarity matrix}
+    """
+    if NThreads > 1:
+        if not Alignment.parpool:
+            Alignment.parpool = PPool(NThreads)
+    #Make a dummy persistence image to figure out resolution
+    ret = getPersistenceImage(np.array([[0, 0]]), [-0.1, 1.1, -0.1, 1.1], res)
+    PIDims = ret['PI'].shape
+    M = SSMA.shape[0]
+    N = SSMB.shape[0]
+    if NThreads > 1:
+        #Turn SSM rows into tuples
+        SSMATup = tuple([SSMA[i, :] for i in range(M)])
+        SSMBTup = tuple([SSMB[i, :] for i in range(N)])
+        args = zip(SSMATup, M*[res])
+        X = Alignment.parpool.map(doIBDTWMergeTreesHelper, args)
+        args = zip(SSMBTup, N*[res])
+        Y = Alignment.parpool.map(doIBDTWMergeTreesHelper, args)
+    else:
+        X = []
+        for i in range(M):
+            X.append(doIBDTWMergeTreesHelper((SSMA[i, :], res)))
+        Y = []
+        for i in range(N):
+            Y.append(doIBDTWMergeTreesHelper((SSMB[i, :], res)))
+    X = np.array(X)
+    Y = np.array(Y)
+    D = getCSM(X, Y)
+    return {'X':X, 'Y':Y, 'D':D, 'PIDims':PIDims}
+    
+
 
 def SMWat(CSM, matchFunction, hvPenalty = -0.2, backtrace = False, backidx = [], animate = False):
     """

ClosedLoops.py
@@ -7,16 +7,17 @@ from Alignment.AlignmentTools import *
 from Alignment.Alignments import *
 from Alignment.DTWGPU import *
 from Alignment.ctw.CTWLib import *
-from Alignment.SyntheticCurves import applyRandomRigidTransformation
+from Alignment.SyntheticCurves import *
 from PaperFigures import makeColorbar
 
-def LoopExperiments(SamplesPerCurve, doPlot = False):
+def LoopExperiments(SamplesPerCurve, Kappa = 0.1, NRelMag = 2, NBumps = 2, doPlot = False):
     np.random.seed(SamplesPerCurve)
     if doPlot:
         plt.figure(figsize=(15, 5))
     NClasses = 7
     CurvesPerClass = 20
     Scores = np.zeros((NClasses, CurvesPerClass, SamplesPerCurve))
+    distortionRatios = []
     for i in range(1, NClasses+1):
         for j in range(1, CurvesPerClass+1):
             for k in range(SamplesPerCurve):
@@ -25,7 +26,13 @@ def LoopExperiments(SamplesPerCurve, doPlot = False):
                 x = x[0::3, :]
                 N = x.shape[0]
                 circshift = np.random.randint(N)
-                y = np.roll(x, circshift, 0)
+                yo = np.roll(x, circshift, 0)
+                
+                (y, Bumps) = addRandomBumps(yo, Kappa, NRelMag, NBumps)
+                diff = np.sqrt(np.sum((yo-y)**2, 1))
+                GHDist = np.max(diff)
+                D = getCSM(y, y)
+                distortionRatios.append(GHDist/np.max(D))
 
                 WarpDict = getWarpDictionary(N)
                 t2 = getWarpingPath(WarpDict, 4, False)
@@ -56,7 +63,7 @@ def LoopExperiments(SamplesPerCurve, doPlot = False):
                 path[:, 1] = np.mod(path[:, 1], len(y))
                 score = computeAlignmentError(PGT, path, etype = 2)
                 Scores[i-1, j-1, k] = score
-                sio.savemat("ClosedLoops.mat", {"Scores":Scores})
+                sio.savemat("ClosedLoops.mat", {"Scores":Scores, "distortionRatios":np.array(distortionRatios)})
                 if doPlot:
                     pathProj = projectPath(path, PCSWM.shape[0], PCSWM.shape[1], 1)
                     #Walk along projected path until we've gone N samples along
@@ -92,4 +99,4 @@ def LoopExperiments(SamplesPerCurve, doPlot = False):
 
 if __name__ == '__main__':
     initParallelAlgorithms()
-    LoopExperiments(10)
+    LoopExperiments(30, doPlot = False)

CoverSongSync.py
@@ -53,6 +53,8 @@ if __name__ == '__main__':
     zoom = 1
     filename1 = "MJ.mp3"
     filename2 = "AAF.mp3"
+    Fs = 22050
+    """
     print "Loading %s..."%filename1
     (XAudio1, Fs) = getAudioLibrosa(filename1)
     print "Loading %s..."%filename2
@@ -60,6 +62,7 @@ if __name__ == '__main__':
 
 
     initParallelAlgorithms()
+    """
     D1 = sio.loadmat("D1.mat")["D"]
     D2 = sio.loadmat("D2.mat")["D"]
     
@@ -82,14 +85,26 @@ if __name__ == '__main__':
 
     (D2, D1) = matchSSMDist(D2, D1)
     
-    plt.subplot(221)
-    plt.imshow(D1, cmap = 'afmhot', interpolation = 'none')
-    plt.subplot(222)
-    plt.imshow(D2, cmap = 'afmhot', interpolation = 'none')
-    plt.subplot(223)
-    plt.plot(D1[0, :])
-    plt.plot(D2[0, :])
-    plt.show()
+    pD1 = scipy.ndimage.interpolation.zoom(D1, 0.2)
+    pD2 = scipy.ndimage.interpolation.zoom(D2, 0.2)
+    floor = 1e-3
+    pD1[pD1 < floor] = floor
+    pD2[pD2 < floor] = floor
+    pD1 = np.log(pD1/floor)
+    pD2 = np.log(pD2/floor)
+    
+    fac = (1.0/0.2)*float(hopSize*winFac)/Fs
+    plt.figure(figsize=(12, 3))
+    plt.subplot(131)
+    plt.imshow(pD1, extent = (0, pD1.shape[0]*fac, pD1.shape[1]*fac, 0), cmap = 'afmhot', interpolation = 'none')
+    plt.xlabel("Time (Sec)")
+    plt.ylabel("Time (Sec)")
+    plt.title("Michael Jackson")
+    plt.subplot(132)
+    plt.imshow(pD2, extent = (0, pD2.shape[0]*fac, pD2.shape[1]*fac, 0), cmap = 'afmhot', interpolation = 'none')
+    plt.title("Alien Ant Farm")
+    plt.xlabel("Time (Sec)")
+    plt.ylabel("Time (Sec)")
     
     """
     CSWM = doIBDTWGPU(D1, D2, returnCSM = True)
@@ -117,11 +132,18 @@ if __name__ == '__main__':
     CSWM = Saved['CSWM']
     path = Saved['path']
     
-    path = makePathStrictlyIncrease(path)
-    plt.imshow(CSWM, cmap = 'afmhot', interpolation = 'none')
-    plt.scatter(path[:, 1], path[:, 0])
-    plt.show()
+    #path = makePathStrictlyIncrease(path)
     
+    fac = float(hopSize*winFac)/Fs
+    plt.subplot(133)
+    plt.imshow(CSWM.T, extent = (0, CSWM.shape[0]*fac, CSWM.shape[1]*fac, 0), cmap = 'afmhot', interpolation = 'none')
+    plt.scatter(path[:, 0]*fac, path[:, 1]*fac, 5, 'm', edgecolor = 'none')
+    plt.title("PCSWM")
+    plt.ylabel("AAF Time (sec)")
+    plt.xlabel("MJ Time (sec)")
+    plt.savefig("CoverSongAlignment.png", bbox_inches = 'tight', dpi = 200)
+    
+    """
     XFinal = np.array([[0, 0]])
     fileprefix = ""
     for i in range(path.shape[0]-1):
@@ -153,6 +175,7 @@ if __name__ == '__main__':
         XFinal = np.concatenate((XFinal, X))
     
     sio.wavfile.write("Synced.wav", Fs, XFinal)
+    """
 
     
     

Jigsaw.py
@@ -161,7 +161,8 @@ if __name__ == '__main__':
     path2 = projectPath(P2, M, N)
     res2 = getProjectedPathParam(path2)
 
-    plt.figure(figsize=(11, 10))
+    plt.figure(figsize=(13, 10*0.66))
+    """
     plt.subplot(321)
     plt.scatter(XL[:, 0], XL[:, 1], 20, np.arange(M), cmap = 'Reds', edgecolor = 'none')
     plt.scatter(XR[:, 0], XR[:, 1], 20, np.arange(N), cmap = 'Blues', edgecolor = 'none')
@@ -179,11 +180,12 @@ if __name__ == '__main__':
     plt.scatter(-4*np.ones(M), np.arange(M), 80, np.arange(M), edgecolor = 'none', cmap = 'Reds')
     plt.xlim([-7, N])
     plt.ylim([M, -7])
-    plt.title("PCSSM")
+    plt.title("PCSWM")
+    """
 
 
 
-    plt.subplot(323)
+    plt.subplot(221)
     plt.scatter(XL[:, 0], XL[:, 1], 20, 'k')
     plt.scatter(XL[path[0, 0]:path[-1, 0]+1, 0], XL[path[0, 0]:path[-1, 0]+1, 1], 20, c = res['C1'], edgecolor = 'none')
     plt.scatter(XR[:, 0], XR[:, 1], 20, 'k')
@@ -194,18 +196,18 @@ if __name__ == '__main__':
     ax = plt.gca()
     ax.set_axis_bgcolor((0.7, 0.7, 0.7))
 
-    plt.subplot(324)
+    plt.subplot(222)
     plt.imshow(CSWM, cmap = 'afmhot', interpolation = 'nearest')
     plt.xlim([-2, N])
     plt.ylim([CSWM.shape[0], -2])
-    plt.title("PCSSM Backtrace 1, Score = %.3g"%score1)
+    plt.title("PCSWM Backtrace 1, Score = %.3g"%score1)
     plt.scatter(P[:, 1], P[:, 0], 30, 'k')
     plt.scatter(path[:, 1], path[:, 0], 20, c = res['C2'], edgecolor = 'none')
 
 
 
 
-    plt.subplot(325)
+    plt.subplot(223)
     plt.scatter(XL[:, 0], XL[:, 1], 20, 'k')
     plt.scatter(XL[path2[0, 0]:path2[-1, 0]+1, 0], XL[path2[0, 0]:path2[-1, 0]+1, 1], 20, c = res2['C1'], edgecolor = 'none')
     plt.scatter(XR[:, 0], XR[:, 1], 20, 'k')
@@ -216,12 +218,14 @@ if __name__ == '__main__':
     ax = plt.gca()
     ax.set_axis_bgcolor((0.7, 0.7, 0.7))
 
-    plt.subplot(326)
+    plt.subplot(224)
     plt.imshow(CSWM, cmap = 'afmhot', interpolation = 'nearest')
     plt.xlim([-2, N])
     plt.ylim([CSWM.shape[0], -2])
-    plt.title("PCSSM Backtrace 2, Score = %.3g"%score2)
+    plt.title("PCSWM Backtrace 2, Score = %.3g"%score2)
     plt.scatter(P2[:, 1], P2[:, 0], 30, 'k')
     plt.scatter(path2[:, 1], path2[:, 0], 20, c = res2['C2'], edgecolor = 'none')
+    plt.xlabel("Piece 1")
+    plt.ylabel("Piece 2")
 
     plt.savefig("Jigsaw.svg", bbox_inches = 'tight')

MOCAP.py
@@ -41,10 +41,10 @@ def MOCAPJumpingJacksExample(doPartial = True, doInterpolated = False):
     DQE = getSSM(XQE.T)
 
     #Load in Weizmann walking mask
-    #(I, IDims) = loadImageIOVideo("MOCAP/jumpingjackscropped.avi")
-    #I = I[25::, :]
-    (I, IDims) = loadImageIOVideo("MOCAP/jumpingjacks2men.ogg")
-    I = I[0:70]
+    (I, IDims) = loadImageIOVideo("MOCAP/jumpingjackscropped.avi")
+    I = I[25::, :]
+    #(I, IDims) = loadImageIOVideo("MOCAP/jumpingjacks2mencropped.avi")
+    #I = I[0:70]
     #I = I[0:int(I.shape[0]/2), :]
     
     if doInterpolated:
@@ -112,13 +112,25 @@ def MOCAPJumpingJacksExample(doPartial = True, doInterpolated = False):
     plt.clf()
     plt.subplot(2, 2, 1)
     plt.imshow(DQ, cmap = 'afmhot', interpolation = 'nearest')
+    plt.xlabel("Frame Number")
+    plt.ylabel("Frame Number")
+    plt.title("MOCAP Quaternion SSM")
     plt.subplot(2, 2, 2)
     plt.imshow(DV, cmap = 'afmhot', interpolation = 'nearest')
+    plt.xlabel("Frame Number")
+    plt.ylabel("Frame Number")
+    plt.title("Video Pixel SSM")
     plt.subplot(2, 2, 3)
     plt.imshow(CSWM, cmap = 'afmhot', interpolation = 'nearest', aspect = 'auto')
+    plt.xlabel("MOCAP Frame Number")
+    plt.ylabel("Video Frame Number") 
+    plt.title("CSWM")
     plt.subplot(2, 2, 4)
     plt.imshow(CSWM, cmap = 'afmhot', interpolation = 'nearest', aspect = 'auto')
     plt.scatter(path[:, 1], path[:, 0], 5, 'c', edgecolor = 'none')
+    plt.xlabel("MOCAP Frame Number")
+    plt.ylabel("Video Frame Number") 
+    plt.title("CSWM + Warping Path")
 
     path = projectPath(path, CSWM.shape[0], CSWM.shape[1], 1)
 
@@ -128,27 +140,30 @@ def MOCAPJumpingJacksExample(doPartial = True, doInterpolated = False):
     plt.figure(figsize=(15, 5))
     for i in range(path.shape[0]):
         plt.clf()
-        plt.subplot(1, 3, 1)
+        plt.subplot(131)
         F = I[path[i, 1], :]
         F = np.reshape(F, IDims)
         plt.imshow(F)
-        plt.title("Frame %i"%path[i, 1])
+        plt.title("Video Frame %i"%path[i, 1])
         plt.axis("off")
 
-        plt.subplot(1, 3, 2)
+        plt.subplot(132)
         F = IM[path[i, 0], :]
         F = np.reshape(F, IMDims)
         plt.imshow(F)
         plt.axis('off')
-        plt.title("Frame %i"%path[i, 0])
-
+        plt.title("MOCAP Frame %i"%path[i, 0])
+        
         plt.subplot(133)
         plt.imshow(CSWM, aspect = 'auto', cmap = 'afmhot', interpolation = 'nearest')
         plt.scatter(path[:, 1], path[:, 0], 5, 'c', edgecolor = 'none')
         plt.scatter(path[i, 1], path[i, 0], 30, 'm')
+        plt.xlabel("MOCAP Frame")
+        plt.ylabel("Video Frame")
+        plt.title("CSWM / Warping Path")
 
         plt.savefig("MOCAPAligned%i.png"%i, bbox_inches = 'tight')
 
 if __name__ == '__main__':
     initParallelAlgorithms()
-    MOCAPJumpingJacksExample(doPartial = False, doInterpolated = False)
+    MOCAPJumpingJacksExample(doPartial = False, doInterpolated = True)

PaperFigures.py
@@ -526,8 +526,9 @@ def SyntheticResults():
     X = np.zeros((N, len(AllErrors)))
     for i in range(len(Types)):
         X[:, i] = AllErrors[Types[i]]
-
-    plt.figure(figsize=(12*0.8, 4*0.8))
+    
+    fac = 0.5
+    plt.figure(figsize=(fac*12, fac*4))
     k = 0
     t = np.linspace(0, 1, 256)
     c = plt.get_cmap('Spectral')
@@ -562,8 +563,9 @@ def WeizmannResults():
     X = np.zeros((N, len(Types)))
     for i in range(len(Types)):
         X[:, i] = AllErrors["P%s"%Types[i]].flatten()
-
-    plt.figure(figsize=(8, 3))
+    
+    fac = 0.8
+    plt.figure(figsize=(fac*4, fac*3))
     plt.boxplot(X, labels = Types)
     plt.ylim([0, 7])
     #plt.ylim([0.1, 10])
@@ -581,7 +583,8 @@ def BUResults():
     for i in range(len(Types)):
         X[:, i] = AllErrors["P%s"%Types[i]].flatten()
     
-    plt.figure(figsize=(8, 3))
+    fac = 0.8
+    plt.figure(figsize=(fac*4, fac*3))
     plt.boxplot(X, labels = Types)
     #plt.yscale('log')
     plt.xlabel("Alignment Algorithm")
@@ -592,7 +595,7 @@ def BUResults():
 def ClosedLoopResults():
     X = sio.loadmat("Results/ClosedLoops.mat")['Scores'].flatten()
     
-    plt.figure(figsize=(8, 3))
+    plt.figure(figsize=(6, 1))
     #plt.boxplot(X)
     #plt.yscale('log')
     #plt.ylabel("Alignment Error")
@@ -600,10 +603,12 @@ def ClosedLoopResults():
     bins = np.logspace(np.min(np.log10(X)), np.max(np.log10(X)), 50)
     
     plt.hist(X, bins = bins)
+    plt.xlim([0.1, 100])
     plt.gca().set_xscale("log")
 
     plt.title("Closed Loop Alignment Errors")
     plt.xlabel("Alignment Errors")
+    plt.ylabel("Counts")
     plt.savefig("ClosedLoops.svg", bbox_inches = 'tight')
 
 if __name__ == '__main__':
@@ -614,6 +619,6 @@ if __name__ == '__main__':
     #Figure8InterpNormalization(100)
     #EulerianInterp(100)
     SyntheticResults()
-    #WeizmannResults()
-    #BUResults()
-    #ClosedLoopResults()
+    WeizmannResults()
+    BUResults()
+    ClosedLoopResults()